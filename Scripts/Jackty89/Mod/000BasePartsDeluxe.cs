07067f60cabe8f146d6721768aa1abb68a58832f
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-04-16 14:55:10 -0600

    Add BasePartsDeluxe

---
+++ Scripts/Jackty89/Mod/000BasePartsDeluxe.cs
@@ -0,0 +1,33 @@
+﻿//=============================================================================
+
+namespace cmk.NMS.Scripts.Mod
+{
+	public class BasePartsDeluxe : cmk.NMS.ModScript
+	{
+		protected override void Execute()
+		{
+			ItemTrees();
+		}
+
+		//...........................................................
+
+		protected void ItemTrees()
+		{
+			var mbin = ExtractMbin<GcUnlockableTrees>("METADATA/REALITY/TABLES/UNLOCKABLEITEMTREES.MBIN");
+			
+			var copyTree = CloneMbin(mbin.Trees[(int)GcUnlockableItemTreeGroups.UnlockableItemTreeEnum.BaseParts]);
+			var copTreeTrees = copyTree.Trees;
+			
+			var basiceBaseParts = mbin.Trees[(int)GcUnlockableItemTreeGroups.UnlockableItemTreeEnum.BasicBaseParts];
+			basiceBaseParts.Trees.Clear();
+			
+			foreach(var tree in copTreeTrees)
+			{
+				basiceBaseParts.Trees.Add(tree);	
+			}
+			
+		}
+	}
+}
+
+//=============================================================================
