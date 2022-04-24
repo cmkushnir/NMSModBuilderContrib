81a149c0948bd8f93ffc33b267e9772223740762
Chris Kushnir <81258013+cmkushnir@users.noreply.github.com>
2022-04-24 13:29:07 -0400

    Add files via upload

---
+++ Scripts/NMSMB/Query/Export_Products.cs
@@ -0,0 +1,23 @@
+﻿//=============================================================================
+// Dump list of all game products to a file.
+//=============================================================================
+
+public class Export_Products : cmk.NMS.Script.QueryClass
+{
+	protected override void Execute()
+	{
+		var save_path = Dialog.SaveFile(null, "Export_Products.log");
+		if( save_path.IsNullOrEmpty() ) return;
+		
+		var file = System.IO.File.CreateText(save_path);
+		
+		foreach( var product in Game.Products.List ) {
+			file.WriteLine($"{product.Id} {product.Name}");
+		}
+		
+		file.Flush();
+		Log.AddSuccess("Finished");
+	}
+}
+
+//=============================================================================
---
+++ Scripts/NMSMB/Query/Search_Scenes.cs
@@ -0,0 +1,44 @@
+﻿//=============================================================================
+// Search all scene mbin's for specific criteria.
+//=============================================================================
+
+public class Search_Scenes : cmk.NMS.Script.QueryClass
+{
+	protected override void Execute()
+	{
+		var save_path = Dialog.SaveFile(null, "Search_Scenes.txt");
+		if( save_path.IsNullOrEmpty() ) return;
+		
+		var file = System.IO.File.CreateText(save_path);
+		
+		var anim_t               = typeof(TkAnimationComponentData);		
+		var TkSceneNodeDataClass = Game.Mbinc.FindClass("TkSceneNodeData");
+		
+		foreach( var mbin_path in TkSceneNodeDataClass.PakItems ) {
+			var scene_mbin = ExtractMbin<TkSceneNodeData>(mbin_path, true, false);
+			if( scene_mbin == null || scene_mbin.Children.IsNullOrEmpty() ) continue;
+			
+			foreach( var child in scene_mbin.Children ) {
+				if( child.Type == "MESH" || child.Attributes.IsNullOrEmpty() ) continue;
+				
+				foreach( var attrib in child.Attributes ) {
+					if( attrib.Name != "ATTACHMENT" || !attrib.Value.Value.EndsWith(".ENTITY.MBIN") ) continue;
+						
+					var entity_mbin = ExtractMbin<TkAttachmentData>(attrib.Value, true, false);
+					if( entity_mbin == null || entity_mbin.Components.IsNullOrEmpty() ) continue;
+					
+					foreach( var component in entity_mbin.Components ) {
+						if( component.GetType() == anim_t ) {
+							file.WriteLine($"{mbin_path} {child.Type.Value} {attrib.Value.Value}");
+						}
+					}
+				}
+			}
+		}
+		
+		file.Flush();
+		Log.AddSuccess("Finished");
+	}
+}
+
+//=============================================================================
