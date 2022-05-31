8abb69ba1970c3c4e50bfa165ee814f42b9a3b0c
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-05-31 08:03:52 -0600

    Create Text-Voice-Language Separation.cs

---
+++ Scripts/Lenni/Text-Voice-Language Separation.cs
@@ -0,0 +1,21 @@
+﻿//=============================================================================
+
+public class Text_Voice_Language_Separation : cmk.NMS.Script.ModClass
+{
+	protected override void Execute()
+	{
+		var english = Game.Languages.Get(LangId.English);
+		
+		foreach( var id in LangId.List ) {
+			if( id == LangId.English ) continue;
+			foreach( var key in english.List ) {
+				SetLanguageText(id, key.Id, key.Text);
+			}
+		}
+	}
+
+	//...........................................................
+}
+
+
+//=============================================================================
