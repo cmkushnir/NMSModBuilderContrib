66502062dbc35523aa664b096d3e4f287054938a
Xander Faern <xander@bladehawke.com>
2022-06-13 07:03:04 -0700

    Formatting fixes, Support cargo slots, Support Sail ships, and Multitool expansion

--- Scripts/Bladehawke/Mod/bFreighter48.cs
+++ Scripts/Bladehawke/Mod/bFreighter48.cs
@@ -3,31 +3,32 @@
 //=============================================================================
 
 
-//	using SizeTypeEnum = GcInventoryLayoutSizeType.SizeTypeEnum;
-	
-	public class bFreighter48 : cmk.NMS.Script.ModClass
-	{
-		protected override void Execute()
-		{
-			GcInventoryTable();
-		}
+public class bFreighter48 : cmk.NMS.Script.ModClass
+{
+  protected override void Execute()
+  {
+    GcInventoryTable();
+  }
 
-		//...........................................................
+  //...........................................................
 
-		protected void GcInventoryTable()
-		{
-			var mbin = ExtractMbin<GcInventoryTable>(
-				"METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN"
-			);
-			var data = mbin.GenerationData.GenerationDataPerSizeType;
-			for (var i = SizeTypeEnum.FreighterSmall; i <= SizeTypeEnum.FreighterLarge; i++) {
-				data[(int)i].MinSlots     = 48;
-				data[(int)i].MaxSlots     = 48;
-				data[(int)i].MinTechSlots = 48;
-				data[(int)i].MaxTechSlots = 48;
-			}
-		}
-	}
+  protected void GcInventoryTable()
+  {
+    var mbin = ExtractMbin<GcInventoryTable>(
+      "METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN"
+    );
+    var data = mbin.GenerationData.GenerationDataPerSizeType;
+    for (var i = SizeTypeEnum.FreighterSmall; i <= SizeTypeEnum.FreighterLarge; i++)
+    {
+      data[(int)i].MinSlots      = 48;
+      data[(int)i].MaxSlots      = 48;
+      data[(int)i].MinTechSlots  = 48;
+      data[(int)i].MaxTechSlots  = 48;
+      data[(int)i].MinCargoSlots = 48;
+      data[(int)i].MaxCargoSlots = 48;
+    }
+  }
+}
 
 
 //=============================================================================
--- Scripts/Bladehawke/Mod/bWeap24.cs
+++ Scripts/Bladehawke/Mod/bWeap24.cs
@@ -2,30 +2,35 @@
 
 //=============================================================================
 
-//using SizeTypeEnum = GcInventoryLayoutSizeType.SizeTypeEnum;
-	
-	public class bWeap24 : cmk.NMS.Script.ModClass
-	{
-		protected override void Execute()
-		{
-			GcInventoryTable();
-		}
+public class bWeap24 : cmk.NMS.Script.ModClass
+{
+  protected override void Execute()
+  {
+    GcInventoryTable();
+  }
 
-		//...........................................................
+  //...........................................................
 
-		protected void GcInventoryTable()
-		{
-			var mbin = ExtractMbin<GcInventoryTable>(
-				"METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN"
-			);
-			var data = mbin.GenerationData.GenerationDataPerSizeType;
-			
-			for (var i = SizeTypeEnum.WeaponSmall; i <= SizeTypeEnum.WeaponLarge; i++) {
-				data[(int)i].MinSlots  = 24;
-				data[(int)i].MaxSlots  = 24;
-			}
-		}
-	}
+  protected void GcInventoryTable()
+  {
+
+    var mbin = ExtractMbin<GcInventoryTable>(
+      "METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN"
+    );
+    var data = mbin.GenerationData.GenerationDataPerSizeType;
+    var maxInv = mbin.WeaponInventoryMaxUpgradeSize.MaxInventoryCapacity;
+
+    for (var i = SizeTypeEnum.WeaponSmall; i <= SizeTypeEnum.WeaponLarge; i++)
+    {
+      data[(int)i].MinSlots = 24;
+      data[(int)i].MaxSlots = 24;
+    }
+    for (var i = InventoryClassEnum.C; i <= InventoryClassEnum.S; i++)
+    {
+      maxInv[(int)i] = 48;
+    }
+  }
+}
 
 
 //=============================================================================
