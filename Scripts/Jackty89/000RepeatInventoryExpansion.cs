1a8d1f66d2353317a418d09bd887c7eaea3aff47
Jackty89 <90223197+Jackty89@users.noreply.github.com>
2022-07-09 11:11:22 +0200

    NMSMB conversion of More Suit Terminal Upgrades
    NMSMB script for https://www.nexusmods.com/nomanssky/mods/2337?tab=files

---
+++ Scripts/Jackty89/000RepeatInventoryExpansion.cs
@@ -0,0 +1,19 @@
+﻿//=============================================================================
+
+public class RepeatInventoryExpansion : cmk.NMS.Script.ModClass
+{
+	protected override void Execute()
+	{
+		var mbin                                 = ExtractMbin<TkAttachmentData>("MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/PARTS/COMMONPARTS/CRYOCHAMBER/ENTITIES/CRYOCHAMBERINTERACTION.ENTITY.MBIN");
+		var interactionComp                      = mbin.Components[2] as GcInteractionComponentData;
+		interactionComp.RepeatInteraction        = true;
+		interactionComp.ReseedAfterRewardSuccess = true;
+		
+		var triggerComp                          = mbin.Components[4] as GcTriggerActionComponentData;
+		triggerComp.States.RemoveAt(2);
+	}
+
+	//...........................................................
+}
+
+//=============================================================================
