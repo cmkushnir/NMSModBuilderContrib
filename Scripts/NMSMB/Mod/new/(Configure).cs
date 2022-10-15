74601639fd02386c998e094ff8246a08297320a5
Chris Kushnir <81258013+cmkushnir@users.noreply.github.com>
2022-10-15 08:38:25 -0400

    Add files via upload

---
+++ Scripts/NMSMB/Mod/new/(Configure).cs
@@ -0,0 +1,58 @@
+﻿//=============================================================================
+
+public class Configure : NMS.Script.ModClass
+{
+	protected override void Execute()
+	{
+		ModFiles.AfterBuildPakStream -= OnAfterBuildPakStream;
+		ModFiles.AfterBuildPakStream += OnAfterBuildPakStream;
+
+		Presets();
+
+		Postsets();
+	}
+
+	//...........................................................
+
+	public override void OnDispose()
+	{
+		ModFiles.AfterBuildPakStream -= OnAfterBuildPakStream;
+	}
+
+	//...........................................................
+
+	protected static void OnAfterBuildPakStream( NMS.Script.ModFiles SCRIPTS, CancellationToken CANCEL )
+	{
+		SCRIPTS.AfterBuildPakStream -= OnAfterBuildPakStream;
+
+		SCRIPTS.ExecuteLog.AddInformation("");
+		SCRIPTS.ExecuteLog.AddHeading("List modified items extracted by multiple scripts");
+
+		foreach( var entry in SCRIPTS.DataCache ) {
+			if( CANCEL.IsCancellationRequested ) break;
+			var data = entry.Data;
+			var mods = entry.Mods;
+			if( !data.IsEdited || mods.Count < 2 ) continue;
+			SCRIPTS.ExecuteLog.AddSuccess(data.Path.Full);
+			foreach( var mod in mods ) {
+				SCRIPTS.ExecuteLog.AddInformation($"\t{mod.Name}");
+			}
+		}
+
+		SCRIPTS.ExecuteLog.AddHeading("Finished listing modified items extracted by multiple scripts");
+	}
+
+	//...........................................................
+
+	protected void Presets()
+	{
+	}
+
+	//...........................................................
+
+	protected void Postsets()
+	{
+	}
+}
+
+//=============================================================================
---
+++ Scripts/NMSMB/Mod/new/Sample.cs
@@ -0,0 +1,15 @@
+﻿//=============================================================================
+
+public class Sample : cmk.NMS.Script.ModClass
+{
+	protected override void Execute()
+	{
+		var mbin = ExtractMbin<GcDebugOptions>(
+			"GCDEBUGOPTIONS.GLOBAL.MBIN"
+		);
+		mbin.SkipIntro = true;  // false
+		mbin.SkipLogos = true;  // false
+	}
+}
+
+//=============================================================================
