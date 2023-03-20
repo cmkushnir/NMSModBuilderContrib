ea038ff90181be16411bad989fdc120e5e5fdcd9
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-20 01:21:57 -0600

    Create [NoBlobs].cs

---
+++ Scripts/JustInNovel/[NoBlobs].cs
@@ -0,0 +1,15 @@
+﻿//=============================================================================
+// You have deployed the Blob bioweapon.
+// They're all dead now. They will not spawn in the game.
+//=============================================================================
+
+public class NoBlobs : cmk.NMS.Script.ModClass
+{
+	protected override void Execute()
+	{
+		Script<Blob_Bioweapon>()         .IsExecutable = true;
+		// - auto-mark ?
+	}
+}
+
+//=============================================================================
